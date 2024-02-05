ad3f14ae2a00892e9237bda029c0723a8decf14e
bumblingbe <79747651+bumblingbe@users.noreply.github.com>
2024-02-06 01:27:08 +1100

    Search functionality for Protein Calculator (not linked to API)

--- src/script-proteinCalculator.js
+++ src/script-proteinCalculator.js
@@ -1,13 +1,49 @@
 //FOOD / PROTEIN CALCULATOR
+////define global variables
+var percentageProtein = 0;
+var foodName = "";
+
 ////define functions
 function handleSearch(event) {
-  event.preventDefault;
+  event.preventDefault();
+
   let foodTypeInputElement = document.querySelector("#food-type-input");
   let foodTypeInputValue = foodTypeInputElement.value;
   alert(`food type: ${foodTypeInputValue}`);
+
   let gramsProteinInputElement = document.querySelector("#grams-protein-input");
   let gramsProteinInputValue = gramsProteinInputElement.value;
   alert(`grams of protein: ${gramsProteinInputValue}`);
+
+  getData(foodTypeInputValue);
+}
+
+function getData(foodTypeInputValue) {
+  let url = `#`;
+  //NEED TO MAKE THIS MORE SPECIFIC DEPENDING ON API INVOLVED
+  axios.get(url).then(retrievePercentageProteinAndFoodName);
+}
+
+function retrievePercentageProteinAndFoodName(response) {
+  percentageProtein = response.data;
+  //NEED TO MAKE THIS MORE SPECIFIC DEPENDING ON API INVOLVED
+
+  foodName = response.data;
+  //NEED TO MAKE THIS MORE SPECIFIC DEPENDING ON API INVOLVED
+
+  updateFoodName(foodName);
+  updateFoodNumbers(percentageProtein);
+}
+
+function updateFoodName(foodName) {
+  let foodNameElement = document.querySelector("#food-name");
+  foodNameElement.innerHTML = foodName;
+}
+
+function updateFoodNumbers(percentageProtein) {
+  let foodWeightValue = percentageProtein * gramsProteinInputValue;
+  let foodWeightElement = document.querySelector("#food-weight");
+  foodWeightElement.innerHTML = `${Math.round(foodWeightValue)}g`;
 }
 
 ////on search, catch input data
